2c37f3c92f6d78224944a0da1a180c8c8d4e1ffb
Philipp Beck <dertieran@gmail.com>
2020-04-06 11:21:25 +0200

    Fix audio from other app stopping after app launch

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -45,7 +45,9 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     }
 
     [session setActive: YES error: nil];
-    [session setCategory:AVAudioSessionCategoryPlayback error:nil];
+
+    // NOTE: Setting this caused the audio (e.g. music) from other apps to stop after the app launch.
+    // [session setCategory:AVAudioSessionCategoryPlayback error:nil];
 }
 
 - (void) parseOptions:(NSDictionary*) options
